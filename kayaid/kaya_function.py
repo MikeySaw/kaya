648c65a2d9be6366f1136d564d82ba318fe91b62
Michael Sawitzki <M.Sawitzki@campus.lmu.de>
2023-10-25 18:46:21 +0200

    implemented the kaya identity function

--- kayaid/kaya_function.py
+++ kayaid/kaya_function.py
@@ -1,4 +1,16 @@
 def kaya_eq(pop, gdp, enInt, carbInt):
+    """
+    Calculate C02 emissions with the kaya identity.
+
+    Args:
+        pop (float): Population size (in millions).
+        gdp (float): GDP per capita (in 1000$/person).
+        enInt (float): Energy intensity (in Gigajoule/1000$GDP).
+        carbInt (float): Carbon intensity (in tonnes CO2/Gigajoule).
+
+    Returns:
+        float: CO2 emissions in million tonnes.
+    """
     return pop * gdp * enInt * carbInt 
     
 
