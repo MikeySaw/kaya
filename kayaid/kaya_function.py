283b444cd7856a1ef69d2628936424f900ae73d4
Michael Sawitzki <M.Sawitzki@campus.lmu.de>
2023-10-26 18:42:18 +0200

    Implemented a Unit Test

--- kayaid/kaya_function.py
+++ kayaid/kaya_function.py
@@ -10,9 +10,19 @@ def kaya_eq(pop, gdp, enInt, carbInt):
 
     Returns:
         float: CO2 emissions in million tonnes.
+
+    Raises:
+        ValueError: If any of the input values is negative
     """
+    if pop < 0 or gdp < 0 or enInt < 0 or carbInt < 0:
+            raise ValueError("Input values must be non-negative!")
     return pop * gdp * enInt * carbInt 
     
 
-germany_co2 = kaya_eq(pop=82.4, gdp=44, enInt=5, carbInt=0.05)
+# test for germany
+germany_co2 = kaya_eq(pop=82.4, gdp=44, enInt=5, carbInt=0.05) 
 print(germany_co2)
+
+# test with negative values
+negative_co2 = kaya_eq(pop=-50, gdp=30, carbInt=-2, enInt=-3)
+print(negative_co2)
